720de5cf46af865f3f0cd691617ab1836c8b6afa
Ankush Agarwal <ankushagarwal11@gmail.com>
2023-09-12 05:47:01 -0700

    Tiktok manual connection cancellation

--- src/components/ConnectTikTok.tsx
+++ src/components/ConnectTikTok.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Button, Text, Input, Box } from "@chakra-ui/react";
+import { Button, Text, Input, Box, Flex } from "@chakra-ui/react";
 import { FaTiktok } from "react-icons/fa";
 
 const ConnectTikTok = () => {
@@ -15,6 +15,10 @@ const ConnectTikTok = () => {
 
   const handleConnectClick = () => {
     setInputVisible(true);
+  };
+
+  const handleCancelClick = () => {
+    setInputVisible(false);
   };
 
   const disconnectTikTok = () => {
@@ -42,17 +46,22 @@ const ConnectTikTok = () => {
           </Button>
         </>
       ) : isInputVisible ? (
-        <Input
-          placeholder='Enter TikTok username'
-          onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
-            if (e.key === "Enter") {
-              const newUsername = (e.target as HTMLInputElement).value;
-              localStorage.setItem("tiktok_username", newUsername);
-              setTiktokUsername(newUsername);
-              setInputVisible(false);
-            }
-          }}
-        />
+        <Flex>
+          <Input
+            placeholder='Enter TikTok username'
+            onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+              if (e.key === "Enter") {
+                const newUsername = (e.target as HTMLInputElement).value;
+                localStorage.setItem("tiktok_username", newUsername);
+                setTiktokUsername(newUsername);
+                setInputVisible(false);
+              }
+            }}
+          />
+          <Button onClick={handleCancelClick} colorScheme='red' marginLeft={2}>
+            Cancel
+          </Button>
+        </Flex>
       ) : (
         <Button
           onClick={handleConnectClick}
